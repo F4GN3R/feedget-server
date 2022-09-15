eb05c38cda19e0d4cc614fb1adbfb2bf81930365
F4GN3R <fagnermorais54@gmail.com>
2022-09-15 16:35:12 -0300

    Feedget server application with database and email service integration

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e3847f6d033b61",
+    pass: "6fa73db89627d4",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Fagner Morais <fagnermorais54@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
